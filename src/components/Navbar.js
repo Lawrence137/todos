8332738e915842162617aefb31c4cc42ade0068a
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-26 13:16:23 +0300

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,12 @@ function Navbar() {
     return ( 
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <NavLink class="navbar-brand" href="#">Task Manager</NavLink>
+    <h3>Task Train</h3>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <NavLink class="nav-link active" aria-current="page" >Home</NavLink>
-        </li>
         <li class="nav-item">
           <NavLink to='/list' class="nav-link" >Tasks</NavLink>
         </li>
@@ -27,10 +24,6 @@ function Navbar() {
         </li>
        
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
